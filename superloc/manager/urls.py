19a3031241a6500c41b4bdb370d6d12174d4de48
Benoit Wiedemann <benoit.wiedemann@dauphine.eu>
2022-01-07 08:59:18 +0100

    Work in progress module gestionnaire

--- superloc/manager/urls.py
+++ superloc/manager/urls.py
@@ -6,19 +6,21 @@ from . import views
 
 app_name = 'manager'
 
-
 def permission_denied_view(request):
     raise PermissionDenied
 
-
 urlpatterns = [
     path('', views.home, name='home'),
+    path('vehicles_availability_agency_choice', views.vehicles_availability_agency_choice, name='vehicles_availability_agency_choice'),
+    path('vehicles_availability/<str:id>', views.vehicles_availability, name='vehicles_availability'),
+    path('vehicles_management_agency_choice', views.vehicles_management_agency_choice, name='vehicles_management_agency_choice'),
+    path('vehicles_management/<str:id>', views.vehicles_management, name='vehicles_management'),
+    path('vehicles_management_vehicle_add', views.vehicles_management_vehicle_add, name='vehicles_management_vehicle_add'),
+]
+"""
     path('logged_out', views.logged_out, name='logged_out'),
     path('login', views.LoginManager.as_view(), name='login'),
     path('logout', auth_views.LogoutView.as_view(),
          {'next_page': 'logged_out'}, name='logout'),
     path('403', permission_denied_view, name='forbidden'),
-    path('vehicles', views.vehicles_home, name='vehicles'),
-    path('agencies', views.agencies_home, name='agencies'),
-    path('customer', views.customers_home, name='customers'),
-]
+"""
--- superloc/superloc_visitor_vehicle.sql
+++ superloc/superloc_visitor_vehicle.sql
@@ -38,7 +38,7 @@ CREATE TABLE `visitor_vehicle` (
   KEY `visitor_vehicle_category_id_ce4b6c0e_fk_visitor_category_id` (`category_id`),
   CONSTRAINT `visitor_vehicle_agency_id_b65b99b8_fk_visitor_agency_id` FOREIGN KEY (`agency_id`) REFERENCES `visitor_agency` (`id`),
   CONSTRAINT `visitor_vehicle_category_id_ce4b6c0e_fk_visitor_category_id` FOREIGN KEY (`category_id`) REFERENCES `visitor_category` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -47,6 +47,7 @@ CREATE TABLE `visitor_vehicle` (
 
 LOCK TABLES `visitor_vehicle` WRITE;
 /*!40000 ALTER TABLE `visitor_vehicle` DISABLE KEYS */;
+INSERT INTO `visitor_vehicle` VALUES (1,'500','Fiat','RV-669-VC','2ef1ref5e21fe51f5',4,1,1),(2,'5008','Peugeot','EV-665-BF','1651fe5e5f15e1fe5',8,11,1),(3,'Megane IV','Renault','GV-585-HG','65efz1ze65f4z6e5f',8,12,1),(4,'3008','Peugeot','AA-001-AA','654D6548DF6486DD4',4,5,1),(5,'3008','Peugeot','AA-002-AA','654D6548DF6486DD5',4,5,1),(6,'3008','Peugeot','AA-008-AA','654D6548DF6486DE8',6,10,1),(7,'3008','Peugeot','AA-009-AA','654D6548DF6486DE9',6,10,1),(8,'Classe E','Mercedes','AA-152-AA','ZZ554Z8516485FRGG',4,13,1),(9,'Espace','Renault','AA-160-AA','MAHAULTW16102010P',4,15,1);
 /*!40000 ALTER TABLE `visitor_vehicle` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -59,4 +60,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-01-04 15:54:26
+-- Dump completed on 2022-01-07  8:56:57

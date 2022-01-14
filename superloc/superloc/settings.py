0478de6d9a5b3429401d402d591d169bb7777e21
BenoitWiedemann <benoit.wiedemann@dauphine.eu>
2022-01-14 21:31:38 +0100

    Correction de l'erreur suite au login dans l'application visitor

--- superloc/superloc/settings.py
+++ superloc/superloc/settings.py
@@ -128,6 +128,7 @@ USE_TZ = False
 STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/category')
 MEDIA_URL = '/media/category/'
+LOGIN_REDIRECT_URL = '/rental'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
